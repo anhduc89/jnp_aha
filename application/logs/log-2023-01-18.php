80ddb7969305d160f5de3119fea3ccc7e7b93e9e
anhduc89 <vaduc89@gmail.com>
2023-01-27 07:54:45 +0700

    xóa log

--- application/logs/log-2023-01-18.php
+++
@@ -1,18 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2023-01-18 10:04:29 --> 404 Page Not Found: %3Cimg%20src=/index
-ERROR - 2023-01-18 10:04:53 --> 404 Page Not Found: %3Cimg%20src=/index
-ERROR - 2023-01-18 13:54:37 --> 404 Page Not Found: JnP_products/che_mu_vo_20190514094344467.html
-ERROR - 2023-01-18 16:13:33 --> Severity: error --> Exception: syntax error, unexpected single-quoted string "">", expecting "," or ";" D:\xampp\htdocs\jnp_aha\application\views\page\jnp_page_product_list.php 43
-ERROR - 2023-01-18 16:13:51 --> Severity: Warning --> Undefined variable $lang D:\xampp\htdocs\jnp_aha\application\views\page\jnp_page_product_list.php 42
-ERROR - 2023-01-18 16:13:51 --> Severity: Warning --> Undefined variable $lang D:\xampp\htdocs\jnp_aha\application\views\page\jnp_page_product_list.php 42
-ERROR - 2023-01-18 16:13:51 --> Severity: Warning --> Undefined variable $lang D:\xampp\htdocs\jnp_aha\application\views\page\jnp_page_product_list.php 42
-ERROR - 2023-01-18 16:13:51 --> Severity: Warning --> Undefined variable $lang D:\xampp\htdocs\jnp_aha\application\views\page\jnp_page_product_list.php 42
-ERROR - 2023-01-18 16:13:51 --> Severity: Warning --> Undefined variable $lang D:\xampp\htdocs\jnp_aha\application\views\page\jnp_page_product_list.php 42
-ERROR - 2023-01-18 16:13:51 --> Severity: Warning --> Undefined variable $lang D:\xampp\htdocs\jnp_aha\application\views\page\jnp_page_product_list.php 42
-ERROR - 2023-01-18 16:34:53 --> Severity: error --> Exception: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near '= 'vn' ORDER BY id DESC' at line 1 D:\xampp\htdocs\jnp_aha\system\database\drivers\mysqli\mysqli_driver.php 305
-ERROR - 2023-01-18 16:35:32 --> Severity: error --> Exception: syntax error, unexpected variable "$lang" D:\xampp\htdocs\jnp_aha\application\models\Model_portfolio.php 27
-ERROR - 2023-01-18 16:47:11 --> Severity: Warning --> Undefined variable $lang D:\xampp\htdocs\jnp_aha\application\views\page\jnp_page_product_detail.php 59
-ERROR - 2023-01-18 16:47:11 --> Severity: Warning --> Undefined variable $lang D:\xampp\htdocs\jnp_aha\application\views\page\jnp_page_product_detail.php 59
-ERROR - 2023-01-18 16:47:11 --> Severity: Warning --> Undefined variable $lang D:\xampp\htdocs\jnp_aha\application\views\page\jnp_page_product_detail.php 59
-ERROR - 2023-01-18 16:47:11 --> Severity: Warning --> Undefined variable $lang D:\xampp\htdocs\jnp_aha\application\views\page\jnp_page_product_detail.php 59
