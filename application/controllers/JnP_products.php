3b1571830cf6d820110184cf887fbc254c5391ba
anhduc89 <vaduc89@gmail.com>
2023-02-28 14:32:04 +0700

    pull code

--- application/controllers/JnP_products.php
+++ application/controllers/JnP_products.php
@@ -18,18 +18,19 @@ class JnP_products extends CI_Controller {
     {
         // $this->session->set_userdata("lang", $this->uri->segment(1));
         $lang =  $this->session->userdata('lang');
+
         // echo 'products '.$lang; exit;
         $this->lang->load('home', $lang == '' ? 'vn' : $lang );
         // echo $this->session->userdata('lang'); exit;
 
         $data['setting'] = $this->Model_common->all_setting();
+        $data['home__products_portfolio']   = $this->Model_portfolio->get_portfolio_data($lang); // danh sách sản phẩm
         // $data['home_about']                 = $this->Model_common->all_page_about(); // về chúng tôi
         // $data['home_categories_portfolio']  = $this->Model_portfolio->get_portfolio_category($lang); // danh mục sản phẩm
-        $data['home__products_portfolio']   = $this->Model_portfolio->get_portfolio_data($lang); // danh sách sản phẩm
         // $data['clients'] 				    = $this->Model_home->all_client(); // khách hàng của chúng tôi
         // $data['testimonials'] 			    = $this->Model_home->all_testimonial();	// nhận xét của khách hàng
 
-        #echo "<pre>"; print_r($data['home__products_portfolio']); exit;
+        #echo "<pre>"; print_r($data); exit;
         $this->load->view('page/jnp_header',$data);
 		$this->load->view('page/jnp_page_product_list',$data);
 		$this->load->view('page/jnp_footer');
---
+++ application/logs/log-2023-02-21.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2023-02-21 13:48:02 --> Severity: Warning --> Undefined variable $setting D:\xampp\htdocs\jnp_aha\application\views\page\jnp_header.php 22
+ERROR - 2023-02-21 13:48:02 --> Severity: Warning --> Trying to access array offset on value of type null D:\xampp\htdocs\jnp_aha\application\views\page\jnp_header.php 22
